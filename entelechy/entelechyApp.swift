e9c344fed81a98fc5a175934665bb4cb8c376684
angel-prieto-pa <angel.prieto.pa@gmail.com>
2025-11-28 20:22:22 -0800

    Initial Commit

---
+++ entelechy/entelechyApp.swift
@@ -0,0 +1,17 @@
+//
+//  entelechyApp.swift
+//  entelechy
+//
+//  Created by Angel Prieto on 11/28/25.
+//
+
+import SwiftUI
+
+@main
+struct entelechyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ entelechyTests/entelechyTests.swift
@@ -0,0 +1,16 @@
+//
+//  entelechyTests.swift
+//  entelechyTests
+//
+//  Created by Angel Prieto on 11/28/25.
+//
+
+import Testing
+
+struct entelechyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
